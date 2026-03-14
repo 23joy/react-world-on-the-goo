66add90e67151c98a8e33ab5ed5eaf714236e5db
23joy <joyshreeroy288@gmail.com>
2026-03-14 09:54:15 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Contries from './assets/components/Countries/Countries'
+
+function App() {
+
+  return (
+    <> 
+    <Contries></Contries>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contries = () => {
+    return (
+        <div>
+            <h2>react on the gooo..</h2>
+        </div>
+    );
+};
+
+export default Contries;
